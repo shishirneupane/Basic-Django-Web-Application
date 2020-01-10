22d816d91cf3b51b0c5a5f01c0661f9c383ba465
Shishir Neupane <58262449+shishirneupane@users.noreply.github.com>
2020-01-11 01:11:09 +0545

    mostly created html files inside templates

--- watchout/watch/views.py
+++ watchout/watch/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def watchhmo(request):
-	return render(request, 'home.html', {'name': 'Ashish Giri'})
+	return render(request, "watch/home.html", {'name': 'Ashish Giri'})
+
+def register(request):
+	return render(request, "watch/register.html", context={})
+
+def login(request):
+	return render(request, "watch/login.html", context={})

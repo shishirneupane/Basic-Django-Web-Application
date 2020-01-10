3511561303685962aca02b58731b791ceee10ea0
Shishir Neupane <58262449+shishirneupane@users.noreply.github.com>
2020-01-10 20:57:43 +0545

    urls files modified

--- watchout/watch/urls.py
+++ watchout/watch/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views
 
-urlpatterns = [
+app_name = "watch"
 
-    path('',views.watchhmo, name='watchhmo')
+urlpatterns = [
+    path('', views.watchhmo, name='watchhmo')
 ]

5137066e6b128aa3c46456ad50eda6b2ede4b002
Shishir Neupane <58262449+shishirneupane@users.noreply.github.com>
2020-01-16 17:50:42 +0545

    changed again in models

--- watchout/watch/admin.py
+++ watchout/watch/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from . import models
+from .import models
 
 admin.site.register(models.Movies)
-admin.site.register(models.Movie_downloader)
-admin.site.register(models.Movie_uploader)
-admin.site.register(models.Moviedirector)
+admin.site.register(models.MovieDownloader)
+admin.site.register(models.MovieUploader)
--- watchout/watch/models.py
+++ watchout/watch/models.py
@@ -25,24 +25,3 @@ class MovieUploader(models.Model):
 
 	def __str__(self):
 		return (self.uploader)
-
-class MovieDirector(models.Model):
-	movie = models.OneToOneField(Movies, on_delete= models.CASCADE)
-	director_name = models.CharField(max_length=50)
-
-	def __str__(self):
-		return (self.director_name)
-
-class Video(models.Model):
-    name= models.CharField(max_length=500)
-    videofile= models.FileField(upload_to='videos/', null=True, verbose_name="")
-
-    def __str__(self):
-        return self.name + ": " + str(self.videofile)
-
-		
-
-
-		
-		
-
